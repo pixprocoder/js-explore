c139dcaa55e1597f0f9b89529bc6f4ba04bb44cc
skpixels <makkobir79@gmail.com>
2023-03-04 14:26:43 +0600

    array-loop added

---
+++ js-fundamental/array-loop.js
@@ -0,0 +1,6 @@
+var numbers = [2, 4, 66, 2, 565, 7, 34, 32];
+
+for (var i = 0; i < numbers.length; i++) {
+  var number = numbers[i];
+  console.log(number);
+}
--- js-fundamental/loops.js
+++ js-fundamental/loops.js
@@ -1,6 +1,20 @@
 // while loop
 var meat = 0;
 while (meat < 5) {
-  console.log(meat, "rost given");
+  // console.log(meat, "rost given");
   meat++;
+}
+
+// for loop
+for (var i = 1; i <= 10; i++) {
+  // console.log(i);
+}
+
+// even numbers using for loop
+for (var i = 0; i <= 20; i += 2) {
+  // console.log(i);
+}
+// odd numbers using for loop
+for (var i = 1; i <= 500; i += 2) {
+  console.log(i);
 }
