f6f34f4984da6c3ab9528e309d504ff53a258ced
skpixels <makkobir79@gmail.com>
2023-03-05 00:59:38 +0600

    leapYear explored

---
+++ apply-js-core/isLeapYear.js
@@ -0,0 +1,13 @@
+function isLeapYear(year) {
+  const leapYear = year % 4;
+  if (leapYear === 0) {
+    return true;
+  }
+  return false;
+}
+
+const myYear = isLeapYear(2135);
+const yourYear = isLeapYear(2000);
+
+console.log("my year is leap year: ", myYear);
+console.log("Your year is leap year: ", yourYear);
