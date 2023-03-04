a2eeec7f63ebf5d9e4e1110d4542287e5eff4e10
skpixels <makkobir79@gmail.com>
2023-03-04 16:11:46 +0600

    isosceles problem solved

---
+++ js-fundamental/practice-problems/isosceles.js
@@ -0,0 +1,11 @@
+// Define the variables
+let side1 = 9;
+let side2 = 8;
+let side3 = 9;
+
+// Check if the triangle is isosceles using if-else statements
+if (side1 === side2 || side2 === side3 || side1 === side3) {
+  console.log("The triangle is isosceles.");
+} else {
+  console.log("The triangle is not isosceles.");
+}
