14092b4ed1b6a55f0f4a81ef828cda5c28e55ded
skpixels <makkobir79@gmail.com>
2023-03-04 15:50:28 +0600

    solved grade problem without using function only used if else

---
+++ js-fundamental/practice-problems/if-else-problem.js
@@ -0,0 +1,89 @@
+// Define the variables
+let myScore = 85;
+let tomScore = 66;
+let janeScore = 95;
+let peterScore = 56;
+let johnScore = 40;
+
+// Calculate the grades for each score
+let myGrade;
+if (myScore >= 90) {
+  myGrade = "A+";
+} else if (myScore >= 80) {
+  myGrade = "A";
+} else if (myScore >= 70) {
+  myGrade = "B";
+} else if (myScore >= 60) {
+  myGrade = "C";
+} else if (myScore >= 50) {
+  myGrade = "D";
+} else {
+  myGrade = "F";
+}
+
+let tomGrade;
+if (tomScore >= 90) {
+  tomGrade = "A+";
+} else if (tomScore >= 80) {
+  tomGrade = "A";
+} else if (tomScore >= 70) {
+  tomGrade = "B";
+} else if (tomScore >= 60) {
+  tomGrade = "C";
+} else if (tomScore >= 50) {
+  tomGrade = "D";
+} else {
+  tomGrade = "F";
+}
+
+let janeGrade;
+if (janeScore >= 90) {
+  janeGrade = "A+";
+} else if (janeScore >= 80) {
+  janeGrade = "A";
+} else if (janeScore >= 70) {
+  janeGrade = "B";
+} else if (janeScore >= 60) {
+  janeGrade = "C";
+} else if (janeScore >= 50) {
+  janeGrade = "D";
+} else {
+  janeGrade = "F";
+}
+
+let peterGrade;
+if (peterScore >= 90) {
+  peterGrade = "A+";
+} else if (peterScore >= 80) {
+  peterGrade = "A";
+} else if (peterScore >= 70) {
+  peterGrade = "B";
+} else if (peterScore >= 60) {
+  peterGrade = "C";
+} else if (peterScore >= 50) {
+  peterGrade = "D";
+} else {
+  peterGrade = "F";
+}
+
+let johnGrade;
+if (johnScore >= 90) {
+  johnGrade = "A+";
+} else if (johnScore >= 80) {
+  johnGrade = "A";
+} else if (johnScore >= 70) {
+  johnGrade = "B";
+} else if (johnScore >= 60) {
+  johnGrade = "C";
+} else if (johnScore >= 50) {
+  johnGrade = "D";
+} else {
+  johnGrade = "F";
+}
+
+// Print the grades to the console
+console.log("My grade is: " + myGrade);
+console.log("Tom's grade is: " + tomGrade);
+console.log("Jane's grade is: " + janeGrade);
+console.log("Peter's grade is: " + peterGrade);
+console.log("John's grade is: " + johnGrade);
