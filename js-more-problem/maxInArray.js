1f29b8ae4f89d4ab82d41d8ad8f229433b94829e
skpixels <makkobir79@gmail.com>
2023-03-05 19:05:32 +0600

    smallest number is found of an array

--- js-more-problem/maxInArray.js
+++ js-more-problem/maxInArray.js
@@ -11,4 +11,20 @@ function getLargest(numbers) {
 
 const numberArray = [112, 11, 443, 44, 112, 54, 114, 203];
 const result = getLargest(numberArray);
-console.log(result);
+// console.log(result);
+
+// Find the smallest number of an array
+function getSmallestNumber(numbers) {
+  let smallest = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element < smallest) {
+      smallest = element;
+    }
+  }
+  return smallest;
+}
+
+const numbers = [112, 11, 443, 44, 112, 54, 114, 203];
+const smallest = getSmallestNumber(numbers);
+console.log("The smallest number is: ", smallest);
