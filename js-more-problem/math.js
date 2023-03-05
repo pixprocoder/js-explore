adc3dedcfab139340df2814fb2f040cb6b2c8c4e
skpixels <makkobir79@gmail.com>
2023-03-05 15:54:21 +0600

    basic math explored

---
+++ js-more-problem/math.js
@@ -0,0 +1,9 @@
+// read from here:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+const number = 334.93;
+
+for (let i = 1; i <= 6; i++) {
+  console.log(Math.round(Math.random(number) * 6));
+  //   console.log(i);
+}
