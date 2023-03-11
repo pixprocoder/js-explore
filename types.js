4fd46d415c080bf65b15bfde4ebe47195fed50f5
skpixels <makkobir79@gmail.com>
2023-03-11 19:21:59 +0600

    tested

--- types.js
+++ types.js
@@ -21,7 +21,7 @@ console.log(typeof name);
 var isHappy = true;
 console.log(typeof isHappy);
 
-// modulus or reminder
+// modulus or reminder hello world
 var price1 = 34;
 var person = 5;
 var sum = price1 % person;
