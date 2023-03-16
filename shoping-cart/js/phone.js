ef7fe161a9e3174cbe3a8e6d121cb7df4b317a46
skpixels <makkobir79@gmail.com>
2023-03-16 20:08:13 +0600

    phone calculation completed

---
+++ shoping-cart/js/phone.js
@@ -0,0 +1,33 @@
+function updatePhoneQuantity(isIncrease) {
+  const phoneNumberField = document.getElementById("phone-input-field");
+  const previousPhoneQuantity = parseInt(phoneNumberField.value);
+  let newPhoneQuantity;
+  if (isIncrease === true) {
+    newPhoneQuantity = previousPhoneQuantity + 1;
+  } else {
+    newPhoneQuantity = previousPhoneQuantity - 1;
+  }
+  phoneNumberField.value = newPhoneQuantity;
+
+  return newPhoneQuantity;
+}
+
+function updateTotalPhonePrice(newPhoneQuantity) {
+  const totalPhonePrice = newPhoneQuantity * 1219;
+  const totalPhonePriceElement = document.getElementById("phone-total-price");
+  totalPhonePriceElement.innerText = totalPhonePrice;
+}
+
+document
+  .getElementById("btn-phone-plus")
+  .addEventListener("click", function () {
+    const newPhoneQuantity = updatePhoneQuantity(true);
+    updateTotalPhonePrice(newPhoneQuantity);
+  });
+
+document
+  .getElementById("btn-phone-minus")
+  .addEventListener("click", function () {
+    const newPhoneQuantity = updatePhoneQuantity(false);
+    updateTotalPhonePrice(newPhoneQuantity);
+  });
