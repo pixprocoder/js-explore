eb69bdc0a7f2b64d7d8c3ab9bf768a3b689f0e0c
skpixels <makkobir79@gmail.com>
2023-03-16 21:58:51 +0600

    pin matcher exploring

---
+++ pin-matcher/js/script.js
@@ -0,0 +1,20 @@
+function getPin() {
+  const pin = generatePin();
+  const pinString = pin + "";
+  if (pinString.length === 4) {
+    return pin;
+  } else {
+    return getPin();
+  }
+}
+
+function generatePin() {
+  const random = Math.round(Math.random() * 10000);
+  return random;
+}
+
+document.getElementById("generate-pin").addEventListener("click", function () {
+  const pin = getPin();
+  const displayPin = document.getElementById("display-pin");
+  displayPin.value = pin;
+});
