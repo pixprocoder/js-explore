fc4701214a7783f8ca95534c87f9ba6140e9ddc9
skpixels <makkobir79@gmail.com>
2023-03-22 19:08:14 +0600

    arrow function explored

--- es6/arrow-function.js
+++ es6/arrow-function.js
@@ -1,2 +1,8 @@
-const add = (num1) => num1 + 2;
-console.log(add(2));
+const add = (num1, num2 = 3) => num1 + num2;
+const result = add(3, 5);
+const temp = `hello the result is: ${result}
+I can easily edit using backtick
+here you go
+`;
+console.log(temp);
+console.log(result);
