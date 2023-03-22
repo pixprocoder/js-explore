0bb3411354ed87ba91820c4eb5cf63bab9c8441e
skpixels <makkobir79@gmail.com>
2023-03-22 19:03:38 +0600

    exploring es6

---
+++ es6/arrow-function.js
@@ -0,0 +1,2 @@
+const add = (num1) => num1 + 2;
+console.log(add(2));
---
+++ es6/task.js
@@ -0,0 +1,9 @@
+// 31-2 ES6 Intro difference between var, Let and const
+// 1.  difference between var, Let and const
+// 2.  history of es6
+
+// 31-3 Function default parameter for not provided values
+
+//  31-5 Arrow function, multiple parameter, function body
+// 1. Function expression vs function declaration
+// 2. difference between regular function vs arrow function
